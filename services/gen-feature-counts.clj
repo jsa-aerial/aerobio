eb89ad16dad61b5e7c30bf51ffb8dd5f3852fd07
jsa-aerial <jsa.aerial@gmail.com>
2017-01-24 15:05:58 -0500

    fix bug not accounting for explicit comparison files!

--- services/gen-feature-counts.clj
+++ services/gen-feature-counts.clj
@@ -4,12 +4,12 @@
 
  :graph {:dir  {:type "func"
                 :name "get-comparison-files"
-                :args ["#1", "#2"]} ; eid rep?
+                :args ["#1", "#2", "#3" ]} ; eid, comparison-file, rep?
          :fcnt {:type "func"
                 :name "featureCounts"
-                :args ["#3", "#4"]} ; feature-type gtf
+                :args ["#3", "#4"]} ; feature-type, gtf
 
          :edges {:dir [:fcnt]}}
 
- :description "Produce 'feature' counts for each pair in the comparison set determined by eid (#1 arg) and whether replicates are used (#2 arg). Requires a gtf (#4 arg) matching the bam reference gbk and feature-type in gtf (#3 arg 'gene', 'CDS', et.al.)."
+ :description "Produce 'feature' counts for each pair in the comparison set determined by eid (#1 arg), the given comparison file for eid (#2 arg) and whether replicates are used (#3 arg). Requires a gtf (#5 arg) matching the bam reference gbk and feature-type in gtf (#4 arg 'gene', 'CDS', et.al.)."
  }
