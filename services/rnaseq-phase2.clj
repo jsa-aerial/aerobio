b1d3b353b5623b31dff62356872dc3cb688e200b
jsa-aerial <jsa.aerial@gmail.com>
2017-01-26 16:04:26 -0500

    Explicitly define flow graph

--- services/rnaseq-phase2.clj
+++ services/rnaseq-phase2.clj
@@ -4,15 +4,17 @@
  :name "rnaseq-phase2",
  :path  "",
 
- :graph {:fcnt {:type "func"
-                :name "gen-feature-counts"
-                :args ["#1" "#2" "#3" "#4" "#5"]
-                }
+ :graph {:dir  {:type "func"
+                :name "get-comparison-files"
+                :args ["#1", "#2", "#3" ]} ; eid, comparison-file, rep?
+         :fcnt {:type "func"
+                :name "featureCounts"
+                :args ["#4", "#5"]} ; feature-type, gtf
          :mail {:type "func"
                 :name "mailit"
                 :args ["#6"]}
-         :edges {:fcnt [:mail]}}
+         :edges {:dir [:fcnt] :fcnt [:mail]}}
 
  ;; instructional data used in /help
- :description "RNASeq flow from asembly files through cummerbund. Process assemblies through to obtain differential analysis."
+ :description "Produce 'feature' counts for each pair in the comparison set determined by eid (#1 arg), the given comparison file for eid (#2 arg) and whether replicates are used (#3 arg). Requires a gtf (#5 arg) matching the bam reference gbk and feature-type in gtf (#4 arg 'gene', 'CDS', et.al.)."
  }
