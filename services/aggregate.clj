092be002f88e2280d16ab1febe68994432c962d0
jsa-aerial <jsa.aerial@gmail.com>
2017-01-26 20:20:12 -0500

    Need an aggregater node; use it to accumulate results for mail in phase2

---
+++ services/aggregate.clj
@@ -0,0 +1,13 @@
+
+{:name "aggregate"
+ :path ""
+ :func (let [result (volatile! [])]
+         (fn[data]
+           (if (pg/eoi? data)
+             @result
+             (do
+               (vswap! result (fn[resval] (conj resval data)))
+               (pg/need)))))
+
+ :description "Streaming aggregation. Takes a stream of data and constructs a vector containing all data. This is mostly for aggregating result maps of upstream nodes. Similar to eager seqs in that all data will be used at once. BE CAREFUL."
+ }
--- services/rnaseq-phase2.clj
+++ services/rnaseq-phase2.clj
@@ -1,5 +1,4 @@
 
-
 {
  :name "rnaseq-phase2",
  :path  "",
@@ -10,10 +9,12 @@
          :fcnt {:type "func"
                 :name "featureCounts"
                 :args ["#4", "#5"]} ; feature-type, gtf
+         :aggr {:type "func"
+                :name "aggregate"}
          :mail {:type "func"
                 :name "mailit"
-                :args ["#6"]}
-         :edges {:dir [:fcnt] :fcnt [:mail]}}
+                :args ["#6" "#7" "#8"]} ; recipient, subject, body intro
+         :edges {:dir [:fcnt] :fcnt [:aggr] :aggr [:mail]}}
 
  ;; instructional data used in /help
  :description "Produce 'feature' counts for each pair in the comparison set determined by eid (#1 arg), the given comparison file for eid (#2 arg) and whether replicates are used (#3 arg). Requires a gtf (#5 arg) matching the bam reference gbk and feature-type in gtf (#4 arg 'gene', 'CDS', et.al.)."
