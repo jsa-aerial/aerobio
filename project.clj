28e16da7a6f5aadfc647349224042aaf89877c8c
jsa-aerial <jsa.aerial@gmail.com>
2017-02-02 19:59:05 -0500

    Adjust for msgpacket repo

--- project.clj
+++ project.clj
@@ -9,7 +9,8 @@
                 *assert* true}
 
   :dependencies
-  [[org.clojure/clojure       "1.7.0"]
+  [[org.clojure/clojure       "1.8.0"]
+   [org.clojure/tools.reader "1.0.0-beta3"]
    [org.clojure/tools.nrepl  "0.2.12"] ; Explicit nREPL
 
    [org.clojure/core.async    "0.1.346.0-17112a-alpha"]
@@ -25,11 +26,12 @@
    [aerial.fs                 "1.1.5"]
    [aerial.utils              "1.2.0"]
    [aerial.bio.utils          "1.1.2"]
+   [aerial.msgpacket          "0.0.1"]
 
    ;;[com.taoensso/sente        "1.4.1"] ; We use new msgpSente
    [com.taoensso/encore      "1.22.0" :exclusions [org.clojure/tools.reader]]
-   [com.taoensso/timbre       "3.3.1"] ; Logger
-   [me.raynes/conch           "0.8.0"] ; streaming shell
+   [com.taoensso/timbre       "3.3.1" :exclusions [org.clojure/tools.reader]]
+   [me.raynes/conch           "0.8.0" :exclusions [org.clojure/tools.reader]]
    [clojure-watch            "0.1.10"] ; watch dir for changes
    [cpath-clj                 "0.1.2"] ; Installation JAR resources access
    [prismatic/schema          "1.0.1"] ; data shape checks for pgm graphs
@@ -38,12 +40,12 @@
 
    [net.apribase/clj-dns      "0.1.0"] ; reverse-dns-lookup
 
-   [compojure                 "1.3.4"] ; Or routing lib of your choice
-   [ring                      "1.3.2"]
-   ;; [ring-anti-forgery      "1.0.0"]
-   [ring/ring-defaults        "0.1.3"] ; Includes `ring-anti-forgery`, etc.
+   [compojure                 "1.3.4" :exclusions [org.clojure/tools.reader]]
+   [ring                      "1.3.2" :exclusions [org.clojure/tools.reader]]
+   [ring/ring-defaults        "0.1.3" :exclusions [org.clojure/tools.reader]]
+   ;; ring-defaults includes [ring-anti-forgery      "1.0.0"]
    [hiccup                    "1.0.5"] ; Optional, just for HTML
-   ;;
+
    ;;; Transit deps optional; may be used to aid perf. of larger data payloads
    ;;; (see reference example for details):
    [com.cognitect/transit-clj  "0.8.259"]
