c6dc53dab5c4c34a69fc86e79a06a0be41cb9a44
jsa-aerial <jsa.aerial@gmail.com>
2017-06-07 20:15:56 -0400

    Account for 3rd param - :NA for wgseq

--- services/wgseq-phase2.clj
+++ services/wgseq-phase2.clj
@@ -5,14 +5,14 @@
 
  :graph {:dir  {:type "func"
                 :name "get-comparison-files"
-                :args ["#1", "#2"]} ; eid, comparison-file, rep?
+                :args ["#1", "#2" "#3"]} ; eid, comparison-file, :NA here
          :bseq {:type "func"
                 :name "breseq-runs"}
          :aggr {:type "func"
                 :name "aggregate"}
          :mail {:type "func"
                 :name "mailp2"
-                :args ["#3"        ; recipient
+                :args ["#4"        ; recipient
                        "Aerobio job status: wgseq phase-2"
                        "Finished"]}      ; subject, body intro
          :edges {:dir [:bseq]
