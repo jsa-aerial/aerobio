b7389a30cc9bfb0f06842591351aa1e01ecac8f0
jsa-aerial <jsa.aerial@gmail.com>
2017-06-13 17:11:20 -0400

    new services to support 'super/global' tnseq aggregation with bottleneck numbers

---
+++ services/tnseq-final-aggregate.clj
@@ -0,0 +1,18 @@
+
+{
+ :name "tnseq-final-aggregate",
+ :path  "",
+
+ :graph {:tnag  {:type "func"
+                 :name "tnseq-global-aggregate"
+                 :args ["#1" "#2" (pg/done)]} ; eid, compfile, Done indicator
+         :mail {:type "func"
+                :name "mailp2"
+                :args ["#3"        ; recipient
+                       "Aerobio job status: tnseq Final Aggregation"
+                       "Finished"]}      ; subject, body intro
+         :edges {:tnag [:mail]}}
+
+ ;; instructional data used in /help
+ :description "Produce gene level aggregate summaries with bottleneck global processing for fitness hits in csvs given  by eid (#1 arg) and the given comparison file for eid (#2 arg)."
+ }
---
+++ services/tnseq-global-aggregate.clj
@@ -0,0 +1,34 @@
+
+{
+ :name "tnseq-global-aggregate",
+ :path "",
+ :func (fn [eid aggrfile data]
+         (when (and (pg/eoi? data) (pg/done? data))
+           (infof "Tn-Seq aggregate starting... %s" data)
+           (let [script (fs/join (fs/pwd) "Scripts" "aggregate.pl")
+                 aggr-tuples (htts/get-aggregate-files eid aggrfile)
+                 refdir (cmn/get-exp-info eid :refs)]
+             (coll/vfold
+              (fn[tuple]
+                (let [[ins ot bnum] tuple
+                      refnm (-> ins first fs/basename
+                                (str/split #"-") first
+                                ((cmn/get-exp-info eid :ncbi-sample-xref)))
+                      refgbk (fs/join refdir (str refnm ".gbk"))
+                      norm (fs/join refdir "NormGenes" (str refnm ".txt"))
+                      ret (apply
+                           pg/perl script
+                           (concat ["-m" norm "-w" "1" "-x" "10"
+                                    "-l" "50" "-b" bnum "-c" refgbk "-o" ot]
+                                   ins [{:verbose true :throw false}]))
+                      exit-code @(ret :exit-code)
+                      exit (if (= 0 exit-code) :success exit-code)
+                      err (-> (ret :stderr) (str/split #"\n") last)]
+                  {:name "tnseq-aggregate"
+                   :value tuple
+                   :exit exit
+                   :err err}))
+              aggr-tuples))))
+
+ :description "Compute global aggregate on tuples [ins ot bnum]. where ins are the set of fitness csvs to aggregate into out csv file ot using bottleneck number bnum. Typically used as post aggregation for in-vivo analysis."
+}
