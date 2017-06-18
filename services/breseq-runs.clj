7bbc564f68c03dbc1b0e1db93a0e5efcebb2f619
jsa-aerial <jsa.aerial@gmail.com>
2017-06-18 14:37:18 -0400

    Change breseq flow so that chunks of 10 run concurrently; only 4 threads per aligner; not read depth limit

--- services/breseq-runs.clj
+++ services/breseq-runs.clj
@@ -2,25 +2,35 @@
 {
  :name "breseq-runs",
  :path "",
- :func (fn [quad]
-         (if (pg/eoi? quad)
+ :func (fn [quads]
+         (if (pg/eoi? quads)
            (pg/done)
-           (let [q1 quad
-                 [[q1r1 q1r2] q1-refgbk q1-outdir] q1
-                 ;;[[q2r1 q2r2] q2-refgbk q2-outdir] q2
-                 poly (if (re-find #"clone" q1-outdir) [] ["-p"])
-                 ret (apply pg/breseq
-                            (concat poly ["-j" "16" "-l" "60"
-                                          "-o" q1-outdir "-r" q1-refgbk
-                                          q1r1 q1r2]
-                                    [{:verbose true :throw false}]))
-                 exit-code @(ret :exit-code)
-                 exit (if (= 0 exit-code) :success exit-code)
-                 err (-> (ret :stderr) (str/split #"\n") last)]
-             {:name "breseq-runs"
-              :value [[q1r1 q1r2] q1-outdir]
-              :exit exit
-              :err err})))
+           (->>
+            quads
+            ;;fire off a set of (count quads) breseq runs
+            (mapv
+             (fn[quad]
+               (future
+                 (let [q1 quad
+                       [[q1r1 q1r2] q1-refgbk q1-outdir] q1
+                       poly (if (re-find #"clone" q1-outdir) [] ["-p"])
+                       ret (apply pg/breseq
+                                  (concat poly ["-j" "4"
+                                                "-o" q1-outdir
+                                                "-r" q1-refgbk
+                                                q1r1 q1r2]
+                                          [{:verbose true :throw false}]))
+                       exit-code @(ret :exit-code)
+                       exit (if (= 0 exit-code) :success exit-code)
+                       err (-> (ret :stderr) (str/split #"\n") last)]
+                   {:name "breseq-runs"
+                    :value [[q1r1 q1r2] q1-outdir]
+                    :exit exit
+                    :err err}))))
+            ;; Wait for them all before proceeding
+            ;; this suffers from partition problems but ensures saner
+            ;; machine resource usage
+            (mapv (fn[fut] (deref fut))))))
 
- :description "Run breseq on WG data (from wgseq-phase0) using polymorphic mode for non-clonal data and consensus mode for straight population data. Currently uses defaults for the switches controlling the processing for these."
+ :description "Run breseq on WG data (from wgseq-phase0) using polymorphic mode for non-clonal data and consensus mode for straight population data. Currently uses defaults for the switches controlling the processing for these. In particular, does not use -l switch which limits read depth! A quad [r1 r2 refgbk otdir] consists of the R1 and R2 paired reads, the reference gbk and the output directory."
 }
--- services/wgseq-phase2.clj
+++ services/wgseq-phase2.clj
@@ -6,6 +6,9 @@
  :graph {:dir  {:type "func"
                 :name "get-comparison-files"
                 :args ["#1", "#2" "#3"]} ; eid, comparison-file, :NA here
+         :take {:type "func"
+                :name "strm-take"
+                :args [10]}
          :bseq {:type "func"
                 :name "breseq-runs"}
          :aggr {:type "func"
@@ -15,7 +18,8 @@
                 :args ["#4"        ; recipient
                        "Aerobio job status: wgseq phase-2"
                        "Finished"]}      ; subject, body intro
-         :edges {:dir [:bseq]
+         :edges {:dir  [:take]
+                 :take [:bseq]
                  :bseq [:aggr]
                  :aggr [:mail]}}
 
