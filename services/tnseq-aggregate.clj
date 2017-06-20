f494a02bf9ce51507b97f4e9614c07654b88b703
jsa-aerial <jsa.aerial@gmail.com>
2017-06-20 16:03:16 -0400

    Guard against bad grp input

--- services/tnseq-aggregate.clj
+++ services/tnseq-aggregate.clj
@@ -10,29 +10,30 @@
                  refdir (cmn/get-exp-info eid :refs)]
              (mapcat
               (fn[grp]
-                (let [pairs (butlast grp)
-                      allins (mapv second pairs)
-                      allcsv (last grp)
-                      strain (-> pairs ffirst fs/basename
-                                 (str/split #"-") first)
-                      refnm  ((cmn/get-exp-info eid :ncbi-sample-xref) strain)
-                      gtf (fs/join refdir (str refnm ".gbk"))
-                      norm   (fs/join refdir "NormGenes" (str refnm ".txt"))]
-                  (coll/vfold
-                   (fn[oiv]
-                     (let [ret (apply
-                                pg/perl script
-                                (concat ["-m" norm "-w" "1" "-x" "10"
-                                         "-l" "50" "-b" "0" "-c" gtf "-o"]
-                                        oiv [{:verbose true :throw false}]))
-                           exit-code @(ret :exit-code)
-                           exit (if (= 0 exit-code) :success exit-code)
-                           err (-> (ret :stderr) (str/split #"\n") last)]
-                       {:name "tnseq-aggregate"
-                        :value oiv
-                        :exit exit
-                        :err err}))
-                   (conj pairs (coll/concatv [allcsv] allins)))))
+                (when (seq grp)
+                  (let [pairs (butlast grp)
+                        allins (mapv second pairs)
+                        allcsv (last grp)
+                        strain (-> pairs ffirst fs/basename
+                                   (str/split #"-") first)
+                        refnm  ((cmn/get-exp-info eid :ncbi-sample-xref) strain)
+                        gtf (fs/join refdir (str refnm ".gbk"))
+                        norm   (fs/join refdir "NormGenes" (str refnm ".txt"))]
+                    (coll/vfold
+                     (fn[oiv]
+                       (let [ret (apply
+                                  pg/perl script
+                                  (concat ["-m" norm "-w" "1" "-x" "10"
+                                           "-l" "50" "-b" "0" "-c" gtf "-o"]
+                                          oiv [{:verbose true :throw false}]))
+                             exit-code @(ret :exit-code)
+                             exit (if (= 0 exit-code) :success exit-code)
+                             err (-> (ret :stderr) (str/split #"\n") last)]
+                         {:name "tnseq-aggregate"
+                          :value oiv
+                          :exit exit
+                          :err err}))
+                     (conj pairs (coll/concatv [allcsv] allins))))))
               grps))))
 
  :description "Function wrapping calc_fitness.py taking cmp-quads [t1 t2 csv ef]."
