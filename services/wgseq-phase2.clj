d9a2891486051f1b0f9121b8256a26a9b3e441aa
jsa-aerial <jsa.aerial@gmail.com>
2017-06-28 18:11:21 -0400

    Move to streaming 'fork/join' for breseq runs

--- services/wgseq-phase2.clj
+++ services/wgseq-phase2.clj
@@ -6,23 +6,16 @@
  :graph {:dir  {:type "func"
                 :name "get-comparison-files"
                 :args ["#1", "#2" "#3"]} ; eid, comparison-file, :NA here
-         :take {:type "func"
-                :name "strm-take"
-                :args [10]}
          :bseq {:type "func"
-                :name "breseq-runs"}
-         :aggr {:type "func"
-                :name "aggregate"}
+                :name "sfj-breseq-runs"}
          :mail {:type "func"
                 :name "mailp2"
                 :args ["#4"        ; recipient
                        "Aerobio job status: wgseq phase-2"
                        "Finished"]}      ; subject, body intro
-         :edges {:dir  [:take]
-                 :take [:bseq]
-                 :bseq [:aggr]
-                 :aggr [:mail]}}
+         :edges {:dir  [:bseq]
+                 :bseq [:mail]}}
 
  ;; instructional data used in /help
- :description ""
+ :description "Run a full set of breseq runs for experiment '#1' EID, with comparison file '#2' and sending completion msg to recipient '#4'. Uses streaming 'fork/join' breseq runner, which is also able to restart run after last completed run in case of server interruptions."
  }
