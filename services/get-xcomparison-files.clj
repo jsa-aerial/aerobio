eb689b7929eea15a833999546e6af18aec54a204
jsa-aerial <jsa.aerial@gmail.com>
2017-12-11 14:10:53 -0500

    add cross experiment comparison capabilities

---
+++ services/get-xcomparison-files.clj
@@ -0,0 +1,22 @@
+
+{:name "get-xcomparison-files"
+ :path ""
+ :repeat true
+ :generator true
+ :func (let [cmpgrps (volatile! :nyi)]
+         (fn[eid comp-filename opt?]
+           (when (= @cmpgrps :nyi)
+             (vswap! cmpgrps
+                     (fn[_]
+                       (cmn/get-xcomparison-files
+                        (cmn/get-exp-info eid :exp)
+                        eid comp-filename opt?))))
+           (let [grp @cmpgrps]
+             (if (seq grp)
+               (let [p (first grp)]
+                 (vswap! cmpgrps (fn[_] (rest grp)))
+                 p)
+               (pg/done)))))
+
+ :description "Streaming comparison input data grp. For RNA-Seq, each element is a vector [bams csv] where bams are the input bams to count and csv is the output file for the count matrix. For Tn-Seq, each element is a quad [t1 t2 csv ef], where t1 and t2 are the condition map files, csv the out matrix file, and ef the expansion factor."
+ }
---
+++ services/rnaseq-xcompare.clj
@@ -0,0 +1,27 @@
+
+{
+ :name "rnaseq-xcompare",
+ :path  "",
+
+ :graph {:dir  {:type "func"
+                :name "get-xcomparison-files"
+                :args ["#1", "#2", "#3" ]} ; eid, comparison-file, rep?
+         :fcnt {:type "func"
+                :name "featureCounts"
+                :args ["#1", "#4", "#5"]} ; eid, feature-type, :NA
+         :dseq {:type "func"
+                :name "deseq2-rnaseq"
+                :args ["#1"]}
+         :aggr {:type "func"
+                :name "aggregate"}
+         :mail {:type "func"
+                :name "mailp2"
+                :args ["#6"        ; recipient
+                       "Aerobio job status: rnaseq X experiment compare"
+                       "Finished"]}      ; subject, body intro
+         :edges {:dir [:fcnt] :fcnt [:dseq]
+                 :dseq [:aggr] :aggr [:mail]}}
+
+ ;; instructional data used in /help
+ :description "Produce 'feature' counts for each pair in the comparison set determined by eid (#1 arg), the given comparison file for eid (#2 arg) and whether replicates are used (#3 arg). Requires a gtf (#5 arg) matching the bam reference gbk and feature-type in gtf (#4 arg 'gene', 'CDS', et.al.). Streams to DESeq2 script to perform DGE analysis and produce several charts"
+ }
