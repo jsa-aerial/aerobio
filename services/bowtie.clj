924bcf3e974ca29532defaf2e3794bc1c93a4e14
jsa-aerial <jsa.aerial@gmail.com>
2019-09-10 11:33:31 -0400

    Add arg set cardinality maps for cmd.config processing

--- services/bowtie.clj
+++ services/bowtie.clj
@@ -1,6 +1,10 @@
 {
  :name "bowtie",
  :path "bowtie",
+
+ :argcard {"-f" 0, "-m" 1, "-M" 1, "-k" 1, "-n" 1
+           "--best" 0, "-y" 0, "-p" 1}
+ 
  ;; instructional data used in /help
  :description  "utility for aligning reads to reference",
 }
--- services/bowtie2.clj
+++ services/bowtie2.clj
@@ -1,6 +1,15 @@
 {
  :name "bowtie2",
  :path "bowtie2",
+
+ :argcard  {"-f" 0, "-q" 0, "--phred33" 0, "--phred64" 0
+            "-D" 1, "-R" 1, "-N" 1, "-L" 1, "-i" 1
+            "--very-fast" 0, "--fast" 0
+            "--sensitive" 0, "--very-sensitive" 0
+            "--very-fast-local" 0, "--fast-local" 0
+            "--sensitive-local" 0, "--very-sensitive-local" 0
+            "--end-to-end" 0, "--local" 0, "-p" 1}
+ 
  ;; instructional data used in /help
  :description  "utility for aligning reads to reference",
 }
