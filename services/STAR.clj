fba994320482154a4b436177f59d9b444a1a9121
jsa-aerial <jsa.aerial@gmail.com>
2019-10-22 10:03:48 -0400

    Add command config argument cardinalities

--- services/STAR.clj
+++ services/STAR.clj
@@ -1,6 +1,16 @@
 {
  :name "STAR",
  :path "STAR",
- ;; instructional data used in /help
+
+ :argcard {"--outSAMunmapped"              1 ; include unmapped reads
+           "--runThreadN"                  1 ; number of threads
+           "--outFileNamePrefix"           1 ; prefix for summary files
+           "--outFilterScoreMinOverLread"  1 ; too short fix
+           "--outFilterMatchNminOverLread" 1 ; too short fix
+           "--outFilterMatchNmin"          1 ; too short fix
+           "--outFilterMismatchNmax"       1 ; too short fix
+           "--readFilesCommand"            1 ; zcat, others
+           }
+
  :description  "splice aware utility for aligning reads to reference",
 }
