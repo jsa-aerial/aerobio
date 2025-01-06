acbea533a933d632473382f4d3a1e30b9be0d24d
jsa-aerial <jsa.aerial@gmail.com>
2025-01-06 11:11:38 -0500

    fix unseparated count in thread switch

--- services/bc2fq.clj
+++ services/bc2fq.clj
@@ -5,10 +5,12 @@
          (let [exp-base (pams/get-params :exp-base)
                expdir (fs/join exp-base eid)
                make (cmn/get-exp-info eid :instrument-make)
-               opts {:illum {:params (conj ["--no-lane-splitting" "--runfolder-dir"] expdir)
+               opts {:illum {:params (conj ["--no-lane-splitting"
+                                            "--runfolder-dir"] expdir)
                              :converter :bcl2fastq}
                      :elembio {:params [expdir (fs/join expdir "Data")
-                                        "--no-projects" "--group-fastq" "--num-threads 32"]}}]
+                                        "--no-projects" "--group-fastq"
+                                        "--num-threads" "32"]}}]
            (infof "BC2FQ on Exp %s" eid)
            (pg/bc2fq make opts)
            eid))
