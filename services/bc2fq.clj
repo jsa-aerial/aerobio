a5c692b809b1d7be38630bba25321d1da7fa0092
jsa-aerial <jsa.aerial@gmail.com>
2025-02-24 12:48:27 -0500

    Enable multi arity for generic runs

--- services/bc2fq.clj
+++ services/bc2fq.clj
@@ -1,19 +1,28 @@
 {
  :name "bc2fq",
  :path "",
- :func (fn [eid & data]
-         (let [exp-base (pams/get-params :exp-base)
-               expdir (fs/join exp-base eid)
-               make (cmn/get-exp-info eid :instrument-make)
-               opts {:illum {:params (conj ["--no-lane-splitting"
-                                            "--runfolder-dir"] expdir)
-                             :converter :bcl2fastq}
-                     :elembio {:params [expdir (fs/join expdir "Data")
-                                        "--no-projects" "--group-fastq"
-                                        "--num-threads" "32"]}}]
-           (infof "BC2FQ on Exp %s" eid)
-           (pg/bc2fq make opts)
-           eid))
+ :func (fn
+         ([eid]
+          (let [exp-base (pams/get-params :exp-base)
+                expdir (fs/join exp-base eid)
+                make (cmn/get-instrument-make eid)
+                opts {:illum {:params (conj ["--no-lane-splitting"
+                                             "--runfolder-dir"] expdir)
+                              :converter :bcl2fastq}
+                      :elembio {:params [expdir (fs/join expdir "Data")
+                                         "--no-projects" "--group-fastq"
+                                         "--num-threads" "32"]}}]
+            (infof "BC2FQ on Exp %s" eid)
+            (pg/bc2fq make opts)
+            eid))
+         ([eid base-dir data-dir & args]
+          (let [exp-base (pams/get-params :exp-base)
+                expdir (fs/join exp-base eid)
+                make (cmn/get-instrument-make eid)]
+            (infof "BC2FQ on base '%s', data dir '%s';\n args '%s'"
+                   base-dir data-dir args)
+            (apply pg/bc2fq make expdir (fs/join expdir "Data") args)
+            eid)))
  ;; instructional data used in /help
  :description  "Function wrapping utility for converting, via dispatch on make, binary files to fastqs",
 }
