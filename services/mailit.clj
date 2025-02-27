7d3bdfb4360168bd59432184ed76174ed17ccc5d
jsa-aerial <jsa.aerial@gmail.com>
2025-02-27 12:04:53 -0500

    reformatting...

--- services/mailit.clj
+++ services/mailit.clj
@@ -1,4 +1,5 @@
 
+
 {
  :name "mailit",
  :path "",
@@ -7,8 +8,13 @@
            (pg/eoi? result) (pg/done)
 
            :else
-           (let [msg (->> result list (cons intro) (map str) (str/join "\n"))]
+           (let [msg (->> result list
+                          (cons intro)
+                          (map str)
+                          (str/join "\n"))]
              (pg/send-msg eid [recipient] subject msg))))
 
- :description "Simple mailer function node. recipient is user account that requested the associated job this node is final pt of DAG, subject is type of job, msg is job finish details."
+ :description "Simple mailer function node. recipient is user account that requested the associated job this node is final pt of DAG, subject is type of job, msg is job
+ finish details."
 }
+
