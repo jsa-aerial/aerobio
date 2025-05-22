1a03ded05d1253410f0dec643aadbbd9408b79e7
jsa-aerial <jsa.aerial@gmail.com>
2025-05-22 13:50:38 -0400

    New job cli support requires cli field

--- Jobs/split-output.clj
+++ Jobs/split-output.clj
@@ -7,5 +7,10 @@
   :prn1 {:type "func",
          :name "prn"}},
  :root :split
- :edges
- {:split [:prn1]}}
+ :edges {:split [:prn1]}
+ :cli {:options
+       [["-u" "--user USERACCNT" "User account (zulip/email) for result msg"
+         :missing "The user account is required"]
+        ["-e" "--eid EID" "The Experiment ID of run"
+         :missing "The Experiment ID is required"]]
+       :order [:user :eid]}}
