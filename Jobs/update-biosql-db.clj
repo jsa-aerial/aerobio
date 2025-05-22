0a99b0da524b59a0b10c01cffe86315cb910475c
jsa-aerial <jsa.aerial@gmail.com>
2025-05-22 15:18:20 -0400

    add job for updating biosql db

---
+++ Jobs/update-biosql-db.clj
@@ -0,0 +1,25 @@
+
+{:nodes
+ {:update
+  {:name "update-biosql-db",
+   :type "func",
+   :args []}
+  :prn1 {:type "func",
+         :name "prn"}},
+ :root :update
+ :edges {:update [:prn1]}
+ :cli {:options
+       [["-i" "--input-dir InputStaging" "Input Staging Directory"
+         :missing "The Input Staging directory is required"]
+        ["-g" "--glob GBKglob" "File regex glob for gbks"
+         :default "*.gbk"]
+        ["-d" "--database DATABASE" "Data Base name"
+         :missing "The Data Base name is required"]
+        ["-p" "--pw PW" "Database Password"
+         :missing "The Database Password is required"]
+        ["-D" "--done DONEDir" "Directory for success gbks"
+         :missing "The Directory for success gbks is required"]
+        ["-E" "--error ERRDir" "Directory for error gbks"
+         :missing "The Directory for error gbks is required"
+         ]]
+       :order [:input-dir :glob :database :pw :done :error]}}
