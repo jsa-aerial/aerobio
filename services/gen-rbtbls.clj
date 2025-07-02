a928f2fddbcabe4b02bc14d4bd8d70a28fa344bf
jsa-aerial <jsa.aerial@gmail.com>
2025-07-02 16:42:26 -0400

    Add a starting infof

--- services/gen-rbtbls.clj
+++ services/gen-rbtbls.clj
@@ -2,6 +2,7 @@
 {:name "gen-rbtbls"
  :path ""
  :func (fn [eid chksz maxn delta minrds]
+         (infof "Start RB table generation: %s" eid)
          (rbtnseq/gen-rbtnseq-xreftbls eid chksz maxn delta minrds))
 
  :description "service indirection to main rbtnseq tbl builder function"}
