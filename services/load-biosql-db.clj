ce9afd242f37f39d603b6f855cde8d32ddd0c052
jsa-aerial <jsa.aerial@gmail.com>
2025-08-13 15:37:11 -0400

    Add aggr and final msg nodes for final job status auto messaging

--- services/load-biosql-db.clj
+++ services/load-biosql-db.clj
@@ -11,8 +11,19 @@
          :move {:type "func"
                 :name "move-processed-file"
                 :args ["#5" "#6"]} ; done-dir err-dir
+         :aggr {:type "func"
+                :name "aggregate"}
+         :msg  {:type "func"
+                :name "mailp2"
+                :args ["load-database"
+                       "#7"         ; recipient
+                       "Aerobio job status: load-biosql-db"
+                       "Finished"]} ; subject, body intro
 
-         :edges {:dir [:load] :load [:move]}}
+         :edges {:dir [:load]
+                 :load [:move]
+                 :move [:aggr]
+                 :aggr [:msg]}}
 
  :description "Load a set of GenBank files (typically from a RefSeq download, but could be others such as HOMD) into an existing biosql schema database. The database needs to have been created already."
  }
