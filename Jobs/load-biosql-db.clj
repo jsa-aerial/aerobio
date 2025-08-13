88f2f95b69c55b799e6cdf73a38e0988484e2101
jsa-aerial <jsa.aerial@gmail.com>
2025-08-13 15:15:13 -0400

    Add user argument - needed for auto messaging

--- Jobs/load-biosql-db.clj
+++ Jobs/load-biosql-db.clj
@@ -13,13 +13,14 @@
          :missing "The Input Staging directory is required"]
         ["-g" "--glob GBKglob" "File regex glob for gbks"
          :default "*.gbk"]
-        ["-d" "--database DATABASE" "Data Base name"
+        ["-d" "--database DBname" "Data Base name"
          :missing "The Data Base name is required"]
-        ["-p" "--pw PW" "Database Password"
+        ["-p" "--pw DBpw" "Database Password"
          :missing "The Database Password is required"]
         ["-D" "--done DONEDir" "Directory for success gbks"
          :missing "The Directory for success gbks is required"]
         ["-E" "--error ERRDir" "Directory for error gbks"
-         :missing "The Directory for error gbks is required"
-         ]]
-       :order [:input-dir :glob :database :pw :done :error]}}
+         :missing "The Directory for error gbks is required"]
+        ["-u" "--user UserAccnt" "User account (zulip/email) for result msg"
+         :missing "The user account is required"]]
+       :order [:input-dir :glob :database :pw :done :error :user]}}
