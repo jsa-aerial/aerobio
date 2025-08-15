150cdaa8e69a57305649f2dfacabfc384a110d6a
jsa-aerial <jsa.aerial@gmail.com>
2025-08-15 18:13:38 -0400

    inverted logic brain cramp

--- services/move-processed-file.clj
+++ services/move-processed-file.clj
@@ -20,8 +20,8 @@
                (warnf "%s: [%s] %s, %s" name exit basename err)
                (infof "%s: [%s] %s" name exit basename))
              (if failed?
-               (fs/move [file] done-dir)
-               (fs/move [file] err-dir))
+               (fs/move [file] err-dir)
+               (fs/move [file] done-dir))
              resmap)))
 
  :description "Move the file given in retmap to either done-dir or err-dir depending on status of upstream processing. retmap is {:name nnn :value vvv :exit eee :err sss} where nnn is name of upstream processor, vvv is the full file spec, eee is either :success or exit code of processor, and sss is a seq of strings (possibly empty) describing error - or warnings."
