feb10ede61503af74da46cff40edebc32a72100f
jsa-aerial <jsa.aerial@gmail.com>
2026-01-21 16:36:43 -0500

    Cleanup config for general use

--- Support/config.clj
+++ Support/config.clj
@@ -1,9 +1,7 @@
 {:scratch-base "/ExpOut"
  :fastq-dirname "Fastq"
  :refdir "/Refs"
- :nextseq-base "/ExpIn"
  :exp-base "/ExpIn"
- :nextseq-fqdir "Data/Intensities/BaseCalls"
  :illum-fqdir "Data/Intensities/BaseCalls"
  :elembio-fqdir "Data/Samples"
 
@@ -18,24 +16,6 @@
  :jobs
  {:dir "~/.aerobio/DBs"
   :file "job-db.clj"}
-
- :biodb-info
- {:genomes
-  {:base "/GenomeSeqs"
-   :refseq46 "RefSeq46"
-   :refseq58 "RefSeq58"
-   :refseq77 "RefSeq77"
-   :tvoseq02 "TVOSeq02"
-   :default :refseq77}
-
-  :blast
-  {:base "/BlastDBs"
-   :refseq58 "RefSeq58/refseq58_microbial_genomic"
-   :refseq77 "RefSeq77/Microbial/refseq77_microbial_complete"
-   :refseq77-arch "RefSeq77/Archaea/refseq77_arhaea_complete"
-   :refseq77-bact "RefSeq77/Bacteria/refseq77_bacteria_complete"
-   :default :refseq77}}
-
 
  :comcfg {:mode :zulip ; or :email or [:zulip :email]
           :email {:accnts {:default "the-default-acct@yourorg.orgtype"
