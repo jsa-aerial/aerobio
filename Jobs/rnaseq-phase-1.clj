11452fd931282642d37ee8be0bd39c6238abe91e
jsa-aerial <jsa.aerial@gmail.com>
2026-04-16 17:09:50 -0400

    Add support for multiple (dispatched) aligners

--- Jobs/rnaseq-phase-1.clj
+++ Jobs/rnaseq-phase-1.clj
@@ -1,10 +1,22 @@
 
-{:nodes
- {:ph1
-  {:name "rnaseq-phase1",
-   :type "tool",
-   :args []}
-  :prn1 {:type "func",
-         :name "prn"}},
- :edges
- {:ph1 [:prn1]}}
+{:bowtie2
+ {:nodes
+  {:ph1
+   {:name "rnaseq-phase1",
+    :type "tool",
+    :args []}
+   :prn1 {:type "func",
+          :name "prn"}},
+  :edges
+  {:ph1 [:prn1]}}
+ :STAR
+ {:nodes
+  {:ph1
+   {:name "rnaseq-star-phase1",
+    :type "tool",
+    :args []}
+   :prn1 {:type "func",
+          :name "prn"}},
+  :edges
+  {:ph1 [:prn1]}}
+ }
