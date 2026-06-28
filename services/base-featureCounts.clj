bc2f435e49fd1c5c81df6a1cbc5147dbfc812a15
jsa-aerial <jsa.aerial@gmail.com>
2026-06-28 16:59:28 -0400

    sync in order to remove

--- services/base-featureCounts.clj
+++ services/base-featureCounts.clj
@@ -5,15 +5,8 @@
  :graph {:fcnt {:type "func"
                 :name "run-featureCounts"
                 :args ["#1" "#2"]} ; options, bam
-         :aggr {:type "func"
-                :name "aggregate"}
-         :endmsg {:type "func"
-                  :name "mailp2"
-                  :args ["bulk-featureCounts"
-                         "#3"            ; user
-                         "Aerobio job status: bulk-featureCounts"
-                         "Finished"]} ; subject, body intro
-         :edges {:fcnt [:aggr] :aggr [:endmsg]}}
+         :prn {:type "func" :name "prn"}
+         :edges {:fcnt [:prn]}}
 
  :description "Produce 'feature' counts for bam(s) (#2 arg) using options (#1 arg) Completion msg sent to user (#3 arg)."
  }
