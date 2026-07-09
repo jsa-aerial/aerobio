af35b10326c21252580050ad4d9cd4332c1b0193
jsa-aerial <jsa.aerial@gmail.com>
2026-07-09 12:26:31 -0400

    Make multi aligner enabled

--- Jobs/tnseq-phase-1.clj
+++ Jobs/tnseq-phase-1.clj
@@ -1,10 +1,22 @@
 
-{:nodes
- {:ph1
-  {:name "tnseq-bowtie1-phase1",
-   :type "tool",
-   :args []}
-  :prn1 {:type "func",
-         :name "prn"}},
- :edges
- {:ph1 [:prn1]}}
+{:bowtie
+ {:nodes
+  {:ph1
+   {:name "tnseq-bowtie1-phase1",
+    :type "tool",
+    :args []}
+   :prn1 {:type "func",
+          :name "prn"}},
+  :edges
+  {:ph1 [:prn1]}}
+
+ :bowtie2
+ {:nodes
+  {:ph1
+   {:name "tnseq-phase1",
+    :type "tool",
+    :args []}
+   :prn1 {:type "func",
+          :name "prn"}},
+  :edges
+  {:ph1 [:prn1]}}}
